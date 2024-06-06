eca665d1a5e589e2a9a9403129167dcb57bdcfa8
Gabrielll04 <gabrielggcarneiro04@gmail.com>
2024-06-05 22:31:00 -0300

    Adding nix script to install packages needed to execute the application

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nodejs_20
+    mysql80
+  ];
+
+  shellHook = ''
+    mkdir ./data
+    mkdir ./mysql_socket
+
+    mysqld --initialize-insecure --console --datadir="$PWD/data" --user="$WHOAMI" 
+
+    mysqld --datadir="$PWD/data" --socket="$PWD/mysql_socket/mysql.sock"
+  '';
+
+  # use mysql.sock in another terminal with mysql80:
+  # mysql -u seu_usuario -p -S your/path/mysql_sockets/mysql.sock
+}
